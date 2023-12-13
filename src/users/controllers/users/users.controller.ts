54a2dcc33f3beb6ac684016bb28764e6b6a7749a
HamzahDb <152789470+hamzah-dayoub@users.noreply.github.com>
2023-12-13 23:34:57 +0400

    🍱 create user controller

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+
+
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    @Get()
+    getUsers() {
+        return " get the user "
+    }
+
+
+    @Post()
+    createUser() {
+        return " Set the user "
+    }
+
+}
+
