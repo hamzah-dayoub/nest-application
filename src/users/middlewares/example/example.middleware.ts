fa654f5ca9163dd7ae3cdd95a01110dd83003e83
HamzahDb <152789470+hamzah-dayoub@users.noreply.github.com>
2023-12-14 22:30:37 +0400

    ✅ test the middleware

--- src/users/middlewares/example/example.middleware.ts
+++ src/users/middlewares/example/example.middleware.ts
@@ -1,9 +1,14 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
 
 @Injectable()
 export class ExampleMiddleware implements NestMiddleware {
-  use(req: any, res: any, next: () => void) {
+  use(req: Request, res: Response, next: NextFunction) {
     console.log("middleware example")
-    next();
+    console.log(req.headers.authorization)
+    const { authorization } = req.headers
+    if (!authorization) throw new HttpException('NO Authorization token', HttpStatus.FORBIDDEN)
+    if (authorization === 'hamzah') next();
+    else throw new HttpException('Invalid Authorization token', HttpStatus.FORBIDDEN)
   }
 }
