941c0a1a3f6d2354519ff098611639e6706a7cdb
HamzahDb <152789470+hamzah-dayoub@users.noreply.github.com>
2023-12-14 17:07:41 +0400

    🍱 initialize user services

--- src/users/services/users/users.service.ts
+++ src/users/services/users/users.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserType } from 'src/users/utiles/types';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    private fakeUsers = [
+        { username: "hamzah", email: "hamzah@gmail.com" },
+        { username: "odai", email: "odai@gmail.com" },
+        { username: "mai", email: "mai@gmail.com" },
+    ]
+    fetchUsers() {
+        return this.fakeUsers
+    }
+
+    createUsers(userDetails: CreateUserType) {
+        console.log(userDetails)
+        return this.fakeUsers.push(userDetails)
+
+
+    }
+
+    fetchUsersById(id: number) {
+        return { id, username: "hamzah", email: "hamzah@gmail.com" }
+    }
+
+
+}
