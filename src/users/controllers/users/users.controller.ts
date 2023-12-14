a0add1dfacbb1cd8c5f6e154d2cd9f71d7c7e6dc
HamzahDb <152789470+hamzah-dayoub@users.noreply.github.com>
2023-12-14 17:09:06 +0400

    ♻️  update user controller

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,14 +1,17 @@
 import { Body, Controller, Get, Param, ParseBoolPipe, ParseIntPipe, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/DTOs/CreateUser.dto';
+import { UsersService } from 'src/users/services/users/users.service';
 
 
 
 @Controller('users')
 export class UsersController {
 
+    constructor(private userService: UsersService) { }
+
     @Get()
-    getUsers(@Query('sortdesc', ParseBoolPipe) sortdesc: boolean) {
-        return " get the user "
+    getUsers() {
+        return this.userService.fetchUsers()
     }
 
 
@@ -16,14 +19,15 @@ export class UsersController {
     @Post('create')
     @UsePipes(new ValidationPipe())
     createUser(@Body() userData: CreateUserDto) {
-        return userData
+        console.log(userData)
+        return this.userService.createUsers(userData)
     }
 
     @Get(':id')
     getUserById(@Param('id', ParseIntPipe) id: number) {
-        console.log(id)
-        console.log(typeof (id))
-        return { id }
+        // console.log(id)
+        // console.log(typeof (id))
+        return this.userService.fetchUsersById(id)
 
     }
 
